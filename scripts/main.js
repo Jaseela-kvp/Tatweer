39e2af2596b9c82428e81a25714e75df99e766c9
Jaseela-kvp <jaseelamohammed00@gmail.com>
2024-07-08 15:07:57 +0400

    floridareal

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,19 @@ function closeNavbar() {
     }
   }
 
+
+// animation 
+ new WOW({
+    boxClass: 'wow', // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset: 100, // distance to the element when triggering the animation (default is 0)
+    mobile: true, // trigger animations on mobile devices (default is true)
+    live: true // act on asynchronously loaded content (default is true)
+  }).init();
+
+
+
+
   // project
   
   function filterProjects(status) {
